8013ef61480c1d55e629810ef1f242137c13b58b
arif_hosen <arif.hosen3888@gmail.com>
2022-12-17 12:49:44 +0600

    missing number optimize solution with math

--- assignment-3/a_3_p_4_leetcode_missing_number.php
+++ assignment-3/a_3_p_4_leetcode_missing_number.php
@@ -5,18 +5,29 @@
 function missingNumber($nums){
     // approach-1: with hashmap
     // time complexity- O(n), space complexity - O(n)
-    $hashMap = [];
-    foreach($nums as $key){
-        if(!isset($hashMap[$key])){
-            $hashMap[$key] = true;
-        }
-    }
+    // $hashMap = [];
+    // foreach($nums as $key){
+    //     if(!isset($hashMap[$key])){
+    //         $hashMap[$key] = true;
+    //     }
+    // }
 
-    for($i = 0; $i <= count($nums); $i++){
-        if(!isset($hashMap[$i])){
-            return $i;
-        }
+    // for($i = 0; $i <= count($nums); $i++){
+    //     if(!isset($hashMap[$i])){
+    //         return $i;
+    //     }
+    // }
+
+    // approach-2: with math equation
+    // time complexity- O(n), space complexity - O(1)
+    // calculate the sum of n th number is (n * (n+1)/2) which means sum = (array.size * (array.size+1)/2)
+    // loop through on array and substract every element from sum, finally return remaining sum which missing numbers
+
+    $sum = (count($nums) * (count($nums) +1)/2);
+    for($i= 0; $i < count($nums); $i++){
+        $sum -= $nums[$i];
     }
+    return $sum;
 }
 
 $result = missingNumber([9,6,4,2,3,5,7,0,1]);
